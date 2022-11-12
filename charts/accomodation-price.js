74cd540be5f2ee3c4374a6ae7d0b656f3451ae5c
Jennifer Tang <yourjennifertang33@hotmail.com>
2022-11-12 17:09:50 +0800

    commit on "main"

--- charts/accomodation-price.js
+++ charts/accomodation-price.js
@@ -1,27 +1,55 @@
-const priceA = {
-    lessThan500: 0,
-    lessThan1000: 0,
-    lessThan2000: 0,
-    lessThan4000: 0,
-    moreThan3999: 0
-  };
+function makeChartForAccommodationPrice(airbnbAccomodationData){
+    const priceA = {
+        lessThan500: 0,
+        lessThan1000: 0,
+        lessThan2000: 0,
+        lessThan4000: 0,
+        moreThan3999: 0
+    };
+    console.log("price A", priceA)
+    console.log("airbnbAccomodationData", airbnbAccomodationData)
 
-
-console.log("price A", priceA)
-console.log("airbnbAccomodationData", airbnbAccomodationData)
-
-  airbnbAccomodationData.forEach(accomodation => {
-    if (accomodation.price < 500) {
-        priceA.lessThan500 += 1;
-    } else if (accomodation.price < 1000) {
-        priceA.lessThan1000 += 1;
-    } else if (accomodation.price < 2000) {
-        priceA.lessThan2000 += 1;
-    } else if (accomodation.price < 4000) {
-        priceA.lessThan4000 += 1;
-    } else if (accomodation.price > 3999) {
-        priceA.moreThan3999 += 1;
-    }
-  });
-
-console.log("price A", priceA)
+    airbnbAccomodationData.forEach(accomodation => {
+        if (accomodation.price < 500) {
+            priceA.lessThan500 += 1;
+        } else if (accomodation.price < 1000) {
+            priceA.lessThan1000 += 1;
+        } else if (accomodation.price < 2000) {
+            priceA.lessThan2000 += 1;
+        } else if (accomodation.price < 4000) {
+            priceA.lessThan4000 += 1;
+        } else if (accomodation.price > 3999) {
+            priceA.moreThan3999 += 1;
+        }
+    })
+    console.log("price A", priceA);
+    Highcharts.chart('accomodation-price-chart', {
+        chart: {
+        type: 'pie'
+        },
+        title: {
+            text: 'Accomodation price'
+        },
+        series: [{
+        name: 'Brands',
+        innerSize: '40%',
+        colorByPoint: true,
+        data: [{
+            name: 'Less than 500',
+            y: priceA.lessThan500,
+        }, {
+            name: 'Less than 1000',
+            y: priceA.lessThan1000
+        }, {
+            name: 'Less than 2000',
+            y: priceA.lessThan2000
+        }, {
+            name: 'Less than 4000',
+            y: priceA.lessThan4000
+        }, {
+            name: 'More than 3999',
+            y: priceA.moreThan3999
+        }]
+    }]
+    })
+}
--- charts/types-of-accommodation.js
+++ charts/types-of-accommodation.js
@@ -1,17 +1,53 @@
-const typeA ={private:0, entire:0, shared:0}
+function makeChartForAccommodationTypes(accommodations){
+    const typeA ={private:0, entire:0, shared:0}
 
-console.log("type A", typeA)
-console.log("airbnbAccomodationData", airbnbAccomodationData)
-
-airbnbAccomodationData.forEach(item=>{
-    if (item.room_type==="Entire home/apt") {
-        typeA.entire=typeA.entire +1;
-    } else if (item.room_type==="Private room") {
-        typeA.private=typeA.private +1;
-    } else if (item.room_type==="Shared room") {
-        typeA.shared=typeA.shared +1;
-    }
-
-})
-
-console.log("type A", typeA)
+    console.log("type A", typeA)
+    console.log("accommodations", accommodations)
+    
+    accommodations.forEach(item=>{
+        if (item.room_type==="Entire home/apt") {
+            typeA.entire=typeA.entire +1;
+        } else if (item.room_type==="Private room") {
+            typeA.private=typeA.private +1;
+        } else if (item.room_type==="Shared room") {
+            typeA.shared=typeA.shared +1;
+        }
+    
+    })
+            
+    console.log("type A", typeA);
+    Highcharts.chart('types-of-accommodation-chart', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Accommodation types'
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: null
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    series: [
+        {
+        name: 'Accommodation types',
+        colorByPoint: true,
+        data: [{
+            name: 'Private room',
+            y: typeA.private
+        },{
+            name: 'Entire home/apt',
+            y: typeA.entire
+        },{
+            name: 'Shared room',
+            y: typeA.shared
+        }]
+    }]
+    })
+}
